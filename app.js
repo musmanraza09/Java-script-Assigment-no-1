2d13676e3ce4b51019a358eacfa61b547a23e2ac
musmanraza09 <musmanraza09@gmail.com>
2025-07-12 02:08:00 +0500

    Javascript Objectives

---
+++ app.js
@@ -0,0 +1,97 @@
+//Assigment no 1
+//task1
+//alert("Welcome to Facebook");
+//task 2
+
+// alert("Error! please enter avalid password");
+
+//task3
+//alert("Welcome to js land... \n Happy coding!");
+//task4
+
+// alert("Welcome to js Land");
+// alert("Happy Coding... \n Prevent this app page from creating additional dialogs");
+
+
+
+//Assigment no 2
+//task 1
+
+//var username;
+
+//tsk 2
+//var myName =" Muhammad Usman Raza";
+//task3
+// task 4
+// var studentName ="Muhammad Usman Raza";
+// var age ="15";
+// var course = "Modern Web Application Development";
+
+// alert(studentName);
+// alert(age + " years old");
+// alert(course);
+//task 6
+// var email = "musmanraza09@gmail.com";
+// alert("My email address is " + email);
+//task7
+// var book = "A smarter Way to learn javascript.";
+// alert("I am trying to learn from the Book " + book)
+//task8
+// document.write("<h1>Yah!! I can write HTML content throuh Javascript</h1>");
+//task9
+// var flower = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+ // alert(flower);
+
+//Assigment no 3
+
+
+//task 1
+//var age = "I am 15 years old";
+//alert(age);
+
+//task 2
+//var visits = "You have visited this site 14 times";
+//alert(visits);
+
+//task 3
+
+//var birthYear = "My birth year is 2009. ";
+
+//document.write(birthYear );
+
+//task 4
+
+//document.write("<p><b>JohnDoe</b> ordered <b>5 T-shirt(s)</b> on XYZ Clothing store.</p>")
+    
+
+
+//Assigment no 4
+
+//task 1 
+
+// var camelCase = "";
+// var snake_case = "";
+// var PascalCase = "";
+ //task 2
+
+ //illegal variable
+ //1:starts with a digit
+ //var 16year = "";
+ //2:giving space or hyphens
+ //var user name user-name = "";
+ //3:invalid characters
+ //var username&age = "";
+ //4:reserved key words
+ //var if = "";
+ //5:starts or end with a period
+
+ //task 3
+//  document.write("<h1>Rules for naming JS variables</h1><br/><br/>");
+//  document.write("<p>variable names can only contain ,numbers,$, and _.For example $my_1stVariable. </p>");
+//  document.write("<p>Variables must begin with a letter,$,or__.For example $name, _name or name.</p>");
+//  document.write("<p>Variable names are case sensitive.</p>");
+//  document.write("<p>Variable names should not be JS Key words.</p>");
+
+
+
+//Assigment no 2
