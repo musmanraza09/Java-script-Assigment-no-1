71e19a159cfddc9360a2f89cf8bed52b0e4960fe
musmanraza09 <musmanraza09@gmail.com>
2025-07-16 11:59:08 +0500

    Java script objectives

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Assigment no 1
+//Assigment no 1 (Chap 01)
 //task1
 //alert("Welcome to Facebook");
 //task 2
@@ -14,7 +14,7 @@
 
 
 
-//Assigment no 2
+//Assigment no 2 (chap 02)
 //task 1
 
 //var username;
@@ -42,7 +42,7 @@
 // var flower = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
  // alert(flower);
 
-//Assigment no 3
+//Assigment no 3 (Chap 03)
 
 
 //task 1
@@ -65,7 +65,7 @@
     
 
 
-//Assigment no 4
+//Assigment no 4 (Chap 04)
 
 //task 1 
 
@@ -94,4 +94,164 @@
 
 
 
-//Assigment no 2
+//Assigment no 5 (chap 05)
+
+//Task 1
+// For Addition
+// var num_1 = 3;
+// var num_2 = 5;
+// var sum = num_1 + num_2 ;
+// document.write("Sum of 3 and 5 is " + sum );
+
+//Task 2
+//For Substraction
+// var num_1 = 3;
+// var num_2 = 5;
+// var substraction = num_1 - num_2 ;
+// document.write("Substraction of 3 and 5 is " + substraction  );
+
+//For Multiplication
+//  var num_1 = 3;
+//  var num_2 = 5;
+//  var multiplication = num_1 * num_2 ;
+//  document.write("Multiplication of 3 and 5 is " + multiplication );
+
+//For Division
+// var num_1 = 3;
+// var num_2 = 5;
+// var division  = num_1 / num_2 ;
+// document.write("Division of 3 and 5 is " + division );
+
+//For Modulus
+//  var num_1 = 3;
+//  var num_2 = 5;
+//  var modulus  = num_1 % num_2 ;
+//  document.write("Reminder of 3 and 5 is " + modulus );
+
+//Task 3
+//var myName ;
+// document.write("Value after variable declaration is: " + myName );
+//   var age = 5 ;
+//   document.write("Initial value: " + age);
+
+
+//   ++age;
+//   document.write("Value after increment is: " + age);
+
+
+//   age += 7;
+//   document.write("Value after addition is: " + age);
+
+//   --age;
+//   document.write("Value after decrement is: " + age);
+
+//   age %= 3;
+//   document.write("The remainder is : " + age);
+
+
+// Task No:04
+
+// var movie_ticket = 600;
+// var total_tickets = 5 ;
+// var result = movie_ticket * total_tickets;
+// document.write("Total cost to buy " + total_tickets + " tickets to a movie is " + result + "PKR");
+
+//Task NO:05
+
+//  var table = 4;
+//  document.write("Table of 4" + "<br>");
+//  document.write(table + " x 1 = " + table*1 + "<br>");
+//  document.write(table + " x 2 = " + table*2 + "<br>");
+//  document.write(table + " x 3 = " + table*3 + "<br>");
+//  document.write(table + " x 4 = " + table*4 + "<br>");
+//  document.write(table + " x 5 = " + table*5 + "<br>");
+//  document.write(table + " x 6 = " + table*6 + "<br>");
+//  document.write(table + " x 7 = " + table*7 + "<br>");
+//  document.write(table + " x 8 = " + table*8 + "<br>");
+//  document.write(table + " x 9 = " + table*9 + "<br>");
+//  document.write(table + " x 10 = " + table*10 + "<br>");
+
+//Task No :06;
+
+//  var celsius = 25;
+//  var convert_far = (celsius * 9/5) + 32;
+
+//  var farenhiet = 70;
+//  var convertToCelsius = ( farenhiet - 32) * 5/9;
+
+//  document.write(celsius + " <sup>0</sup>C is " + convert_far + " <sup>0</sup>F <br>");
+//  document.write(farenhiet + " <sup>0</sup>F is " + convertToCelsius + " <sup>0</sup>C <br>");
+
+
+//Task No : 07
+
+// var item1_price = 650;
+// var item2_price = 100;
+// var item1_quantity = 3;
+// var item2_quantity = 7;
+// var shipping_charges = 100;
+// var total_price = item1_price * item1_quantity + item2_price * item2_quantity + shipping_charges ;
+
+// document.write("<h1>Shopping Cart</h1><br/><br/><br/>");
+// document.write("Price of item1 is " +  item1_price + "<br/>");
+// document.write("Quantity of item 1 is " + item1_quantity + "<br/>");
+// document.write("Price of item2 is " + item2_price + "<br/>");
+// document.write("Quantity of item2 is " + item2_quantity + "<br/>");
+// document.write("Shipping Charges is " + shipping_charges + "<br/><br/>");
+// document.write("Total cost of your order is " + total_price + "<br/>");
+
+
+
+//Task NO  :08
+
+// var total_marks = 550;
+// var obtained_marks = 500;
+// var your_percentage = obtained_marks / total_marks * 100;
+// document.write("<h1>Mark sheet</h1><br/><br/><br/>");
+// document.write("Total Marks is : " + total_marks );
+// document.write("Obtained Marks is : " + obtained_marks  );
+// document.write("Your Total % is :" + your_percentage );
+  
+//Task No : 09
+
+//document.write("Currency in PKR <br/><br/></br>");
+//document.write("Total currency in PKR : 1784");
+
+  
+//Task NO :10
+
+// var number = 7;
+// var calculation = ((number + 5)*10);
+// document.write("Answer is : " + calculation);
+
+//Task no 11
+
+
+// var current_year = 2025;
+// var birth_year = 2009;
+// var age = current_year - birth_year; 
+// document.write("<h1>Age Calculator<h1/><br/><br/><br/>");
+// document.write("Current Year : " + current_year + "<br/>" );
+// document.write("Birth Year : " + birth_year + "<br/>" );
+// document.write("YOur Age : " + age + "<br/>" );
+
+//Task no 12
+
+// document.write("<h1>The Geometrizer<h1/> <br/><br/><br/>");
+// document.write("Radius of a circle : 20 <br/>");
+// document.write("The circumfrence is : 125.6799999999<br/>");
+// document.write("The Area is :1256.8");
+
+// Task no 13
+
+
+// document.write("<h1>The Life Time Supply Calculator <h1/><br/><br/>");
+// document.write("Favourite snack:Choclate chip <br/>");
+// document.write("Current Age : 16 <br/>");
+// document.write("Estimated Maximum Age : 65");
+// document.write("Amount of snacks per day:3 <br/>");
+// document.write("You will neeed 150 choclate chip to last you until yhe ripe gold age of 65");
+
+
+//Task no 
+
